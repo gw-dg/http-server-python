788448e5a3469398abd8e2103a9935e760dd7b28
gw-dg <92924837+gw-dg@users.noreply.github.com>
2024-06-18 15:51:44 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,16 +1,31 @@
 import socket
 
-
+def extract_path(request):
+    lines = request.splitlines()
+    if lines:
+        request_line = lines[0].strip()
+        parts = request_line.split()
+        if len(parts)>=2:
+            return parts[1]
+    return None;
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    #server_socket.accept() 
+
     while True:
         client_socket, client_address = server_socket.accept()
-        response = "HTTP/1.1 200 OK\r\n\r\n"
-        client_socket.sendall(response.encode('utf-8'))
+        request = client_socket.recv(1024).decode('utf-8')
+        path = extract_path(request)
+        if path == '/':
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+            client_socket.sendall(response.encode('utf-8'))
+
+        else: 
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
+            client_socket.sendall(response.encode('utf-8'))
+    
         client_socket.close()
 
 
