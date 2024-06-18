2aee5dd11148bec797f27a57d283bc1b4251f14d
gw-dg <92924837+gw-dg@users.noreply.github.com>
2024-06-18 15:14:39 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() 
+    #server_socket.accept() 
     while True:
         client_socket, client_address = server_socket.accept()
         response = "HTTP/1.1 200 OK\r\n\r\n"
