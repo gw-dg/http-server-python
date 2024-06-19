d1f2af56c638dd1098aaa96b46f93ffb2cbb5a58
gw-dg <92924837+gw-dg@users.noreply.github.com>
2024-06-19 15:31:49 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 import socket
 
 def extract_path(request):
-    lines = request.splitlines()
-    response_body = ""
-    for i in range(len(lines)):
-        if lines[i].find("User-Agent") != -1 :
-            response_body = lines[i]
-            break;
-    resp_cont = response_body.split();
-    if response_body=="":
-        return ""
-    else: 
-        return resp_cont[1];
+    data = request.split("\r\n")
+    path = data[0].split(" ")[1]
+    response = ""
+    if path == "/":
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+    elif path.startswith("/echo"):
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
+    elif path.startswith("/user-agent"):
+        content = data[0].split(" ")[1]
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(content)}\r\n\r\n{content}"
+    else :
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    return response
+    
     
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -22,16 +25,8 @@ def main():
     while True:
         client_socket, client_address = server_socket.accept()
         request = client_socket.recv(1024).decode('utf-8')
-        content = extract_path(request)
-        length = len(content)
-        status = "200 OK"
-        response = "HTTP/1.1 " + status+ "\r\nContent-Type: text/plain\r\nContent-Length: " + str(length)+"\r\n\r\n"+content
-        
-        
+        response = extract_path(request)
         client_socket.sendall(response.encode('utf-8'))
-
-        
-    
         client_socket.close()
 
 
